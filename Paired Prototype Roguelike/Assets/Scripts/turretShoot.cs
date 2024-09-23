409f1b3cd96ebf00ff8adf7d006f92492d091493
YuehengHan <hanyuehe@usc.edu>
2024-09-22 20:42:23 -0700

    update turret

---
+++ Paired Prototype Roguelike/Assets/Scripts/turretShoot.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class turretShoot : MonoBehaviour
+{   
+    private float timeSinceLastShot = 0.0f;
+    [SerializeField] float fireRate = 5.0f;
+    [SerializeField] GameObject projectile;
+    [SerializeField] GameObject gunBarrel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Shoot();
+    }
+
+    void FixedUpdate()
+    {
+        timeSinceLastShot += Time.fixedDeltaTime;
+    }
+
+    private void Shoot()
+    {
+        if ((timeSinceLastShot > 1 / fireRate) && projectile && gunBarrel)
+        {
+            var bullet = Instantiate(projectile, gunBarrel.transform.position, gunBarrel.transform.rotation);
+            timeSinceLastShot = 0;
+        }
+    }
+}

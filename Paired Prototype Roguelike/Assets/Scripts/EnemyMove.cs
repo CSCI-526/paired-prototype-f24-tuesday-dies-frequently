bdb2f18313b51df58f54b5a4cad9680ffe30a7bf
YuehengHan <hanyuehe@usc.edu>
2024-09-23 15:23:00 -0700

    update wall

--- Paired Prototype Roguelike/Assets/Scripts/EnemyMove.cs
+++ Paired Prototype Roguelike/Assets/Scripts/EnemyMove.cs
@@ -56,6 +56,10 @@ public class EnemyMove : MonoBehaviour
         else if(collision.gameObject.CompareTag("Player"))
         {
             collision.gameObject.GetComponent<PlayerHealth>().TakeDamage();
+        }else if (collision.gameObject.CompareTag("Wall"))
+        {
+            collision.gameObject.GetComponent<DefensiveWallHealth>().TakeDamage();
+            GetComponent<EnemyHealth>().Die();
         }
     }
 }

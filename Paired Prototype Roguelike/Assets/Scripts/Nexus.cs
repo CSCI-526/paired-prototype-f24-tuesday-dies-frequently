6d0e43af7ee394e142273488aebe2ea59ed9d641
Dylon Shiao <dshiao@usc.edu>
2024-09-30 17:56:19 -0700

    nexus now also spawns XP over time

--- Paired Prototype Roguelike/Assets/Scripts/Nexus.cs
+++ Paired Prototype Roguelike/Assets/Scripts/Nexus.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class Nexus : MonoBehaviour
 {
+    [SerializeField] public bool spawnXP;
     [SerializeField] public int health;
     [SerializeField] public int maxHealth;
+    [SerializeField] public float xpSpawnInterval;
+    
+    [SerializeField] protected GameObject XP;
+    [SerializeField] public Vector3 xpSpawnOffset; 
+
+    private float timeSinceLastSpawn = 0.0f;
 
     // Start is called before the first frame update
     private void Start()
@@ -21,6 +28,19 @@ public class Nexus : MonoBehaviour
         { 
             GameManager.Instance.TriggerGameOver();
             gameObject.SetActive(false);
+        }
+    }
+
+    public void Update()
+    {
+        if(spawnXP)
+        {
+            timeSinceLastSpawn += Time.deltaTime;
+            if (timeSinceLastSpawn > xpSpawnInterval)
+            {
+                timeSinceLastSpawn = 0.0f;
+                Instantiate(XP, transform.position + xpSpawnOffset, Quaternion.identity);
+            }
         }
     }
 
